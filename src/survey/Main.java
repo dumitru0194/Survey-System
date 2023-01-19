1cb513b202cb60837a989fc7fb91f985dc86d5f7
dumitru0914 <pintilied41@gmail.com>
2023-01-19 15:09:49 +0200

    project done

--- src/survey/Main.java
+++ src/survey/Main.java
@@ -2,10 +2,9 @@ package survey;
 
 import java.sql.SQLException;
 
-
 public class Main {
-    public static void main(String[] args)throws SQLException {
+    public static void main(String args[]) throws SQLException {
         Login login = new Login();
         login.loginView();
-    }
+	}
 }
